66c3c08e46e834a3d581a191989e317c585fc295
Nelson Tapia <ntapiapinto@gmail.com>
2023-02-24 23:04:56 -0300

    se mejora extension keyboardDisplayable

--- PaymantApp/Commons/Components/KeyBoardDisplayable.swift
+++ PaymantApp/Commons/Components/KeyBoardDisplayable.swift
@@ -12,22 +12,62 @@ protocol KeyBoardDisplayable: AnyObject {
 }
 
 extension KeyBoardDisplayable where Self : UIViewController {
-    
     func configKeyboradSuscription(mainScrollView: UIScrollView) {
+        
+        keyboardWillShow(mainScrollView: mainScrollView)
+        keyboardWillHideNotification(mainScrollView: mainScrollView)
+        hideKeyboardsWithScrollView(mainScrollView: mainScrollView)
+    }
+    
+    private func keyboardWillShow(mainScrollView: UIScrollView)  {
+        NotificationCenter
+        .default
+        .publisher(for: UIResponder.keyboardWillShowNotification)
+        .sink { [weak self]notification in
+            self?.addExtraSpaceToScrollView(mainScrollView: mainScrollView, notification: notification)
+        }.store(in: &cancellableBag)
+    }
+    
+    private func addExtraSpaceToScrollView(
+        mainScrollView: UIScrollView,
+        notification: NotificationCenter.Publisher.Output
+    ){
+        let keyboardFrameEndUserInfoKey = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey]
+        let keyboardSize = (keyboardFrameEndUserInfoKey as? NSValue)?.cgRectValue
+        
+        guard let keyboardSafeSize = keyboardSize else { return }
+        addSpaceToScrollView(withScroll: mainScrollView, withSize: keyboardSafeSize)
+    }
+    
+    private func addSpaceToScrollView(withScroll mainScrollView: UIScrollView, withSize keyboardSafeSize: CGRect) {
+        let safeAreaBottom = view.safeAreaInsets.bottom
+        
+        let contentInset = UIEdgeInsets(
+            top: 0,
+            left: 0,
+            bottom: keyboardSafeSize.height - safeAreaBottom,
+            right: 0)
+        
+        mainScrollView.contentInset = contentInset
+        mainScrollView.scrollIndicatorInsets = contentInset
+       
+    }
+    
+    private func hideKeyboardsWithScrollView(mainScrollView: UIScrollView) {
+        mainScrollView.keyboardDismissMode = .interactive
+    }
+    
+    private func keyboardWillHideNotification (mainScrollView: UIScrollView) {
         NotificationCenter
             .default
-            .publisher(for: UIResponder.keyboardWillShowNotification)
-            .sink { notification in
-                
-                let keyboardFrameEndUserInfoKey = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey]
-                let keyboardSize = (keyboardFrameEndUserInfoKey as? NSValue)?.cgRectValue
-                
-                guard let keyboardSafeSize = keyboardSize else { return }
-                
-                let contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSafeSize.height, right: 0)
-                
-                mainScrollView.contentInset = contentInset
-                
+            .publisher(for: UIResponder.keyboardWillHideNotification)
+            .sink { [weak self]notification in
+                self?.resetScrollView(mainScrollView: mainScrollView)
             }.store(in: &cancellableBag)
+    }
+    
+    private func resetScrollView(mainScrollView: UIScrollView){
+        mainScrollView.contentInset = .zero
+        mainScrollView.scrollIndicatorInsets = .zero
     }
 }
